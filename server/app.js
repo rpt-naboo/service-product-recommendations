2f085db127cdf697e70cf0a9b184965594a3ff58
montekaka <josh.chen.jia@gmail.com>
2018-10-13 13:44:00 -0700

    setup an endpoint to test the product create action

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3007;
-const db = require('../db');
+const db = require('../db/index.js');
+
+app.get('/api', (req, res)=>{
+	//const data = new db.ProductModel({name: 'GG'});
+	db.productCreate({name: 'Josh'}, (err, res) => {
+		console.log(err, res)
+	})
+	res.send('Hello world!');
+});
 
 app.get('/', (req, res) => {
 	res.send('Hello world!');
 });
 
-
 app.listen(port, () => {
 	console.log(`Exapmle app listening on port ${port}!`);
 })
+
