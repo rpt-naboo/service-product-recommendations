9e2fe88a7462e32a4f9c89775f9acc3ccd8d07de
montekaka <josh.chen.jia@gmail.com>
2018-10-20 15:55:10 -0700

    add eventhandler to get the size of the item

--- client/src/components/suggest-list/suggest.jsx
+++ client/src/components/suggest-list/suggest.jsx
@@ -11,21 +11,31 @@ class Suggest extends React.Component {
     this.state = {
     	'text': 'Suggests',
       'id': '5bc966caa6944b44e5edf886',
-      'data': []
+      'data': [],
+      'widgetWidth': 0
     }
     this.get = this.get.bind(this);
   }
 
   componentDidMount() {
-    this.get();
+    this.get();    
+    this.handleResize();
+  }
+
+  handleResize() {
+    window.addEventListener("resize", () => {
+      const widgetWidth = document.getElementById('widget').clientWidth;
+      console.log(widgetWidth)
+    })
   }
 
   get() {
     const _this = this;
     const id = _this.state.id;
+    const widgetWidth = document.getElementById('widget').clientWidth;
     axios.get(GET_PATH+id)
     .then((res) => {
-      _this.setState({data: res.data})
+      _this.setState({data: res.data, widgetWidth: widgetWidth})
     })
     .catch((err) => {
       console.log(err);
@@ -36,7 +46,7 @@ class Suggest extends React.Component {
     return (
       <div>
         <h1>Total suggest items: {this.state.data.length}</h1>
-        <div>
+        <div id="widget">
           {
             this.state.data.map((item) => 
               <SuggestItem item={item.suggestedProduct} key={item._id}/>
