11ce42a1e28c0b0deda7227794353439e89aa062
montekaka <josh.chen.jia@gmail.com>
2018-10-20 12:18:11 -0700

    clean up the test for client endpoint

--- server/app.test.js
+++ server/app.test.js
@@ -4,7 +4,6 @@ const app = require('./app.js');
 describe('Gets api/suggestProducts/id', () => {
 	it('should return suggests products', async () => {
 		const res = await request(app).get('/api/suggestProducts/5bc966caa6944b44e5edf886');
-		console.log(res.body.length)
 		expect((res.body.length)).toBe(9);
 	});
 });
