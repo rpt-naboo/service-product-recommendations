19b1a2dbb405f42529e86ec7cf80e6c7781a397c
montekaka <josh.chen.jia@gmail.com>
2018-10-20 15:10:56 -0700

    fetch the suggest prodocuts from the endpoint

--- client/src/components/suggest-list/suggest.jsx
+++ client/src/components/suggest-list/suggest.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+const GET_PATH = '/api/suggestProducts/';
 
 class Suggest extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    	'text': 'Suggests'
+    	'text': 'Suggests',
+      'id': '5bc966caa6944b44e5edf886',
+      'data': []
     }
+    this.get = this.get.bind(this);
+  }
+
+  componentDidMount() {
+    this.get();
+  }
+
+  get() {
+    const _this = this;
+    const id = _this.state.id;
+    axios.get(GET_PATH+id)
+    .then((res) => {
+      _this.setState({data: res.data})
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 
   render() {
     return (
-      <div>{this.state.text}</div>
+      <div>Total suggest items: {this.state.data.length}</div>
     );
   }
 }
