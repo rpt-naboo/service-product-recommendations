1d32ca31364ac4fd95df93fe5ce90345200e3570
montekaka <josh.chen.jia@gmail.com>
2018-10-20 12:17:46 -0700

    config webpack to render react

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    rules : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        exclude: /(node_modules|bower_components)/,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['@babel/preset-react']
+       }
+      }
+    ]
+  }
+};
