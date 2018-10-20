97209bebf50e74568f2f31d16dbdbe4711be8d86
montekaka <josh.chen.jia@gmail.com>
2018-10-20 15:55:48 -0700

    clean up the file

--- db/index.js
+++ db/index.js
@@ -1,18 +1,22 @@
-//Import the mongoose module
-const mongoose = require('mongoose');
 const mongoDB = 'mongodb://127.0.0.1/service_product_recommendations';
+
+const mongoose = require('mongoose');
 const ProductModel = require('./models/product.js');
 const SuggestProductModel = require('./models/suggestProduct.js');
 
 const promise = mongoose.connect(mongoDB);
 
 promise.then((db) => {
-  console.log('woohoo mongoose connected successfully');
+  if (db.connections.length) {
+    console.log('woohoo mongoose connected successfully');
+  }  
 }).catch((err) => {
-  console.log('mongoose connection error, please make sure your mongodb is running.');  
+  if (err) {
+    console.log('mongoose connection error, please make sure your mongodb is running.');
+  }  
 });
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 const Product = mongoose.model('Product', ProductModel.schema);
 const SuggestProduct = mongoose.model('SuggestProduct', SuggestProductModel.schema);
