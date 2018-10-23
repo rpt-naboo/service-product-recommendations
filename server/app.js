d5d6eff0cb0e952a794abe3a160e690b1dcc965b
montekaka <josh.chen.jia@gmail.com>
2018-10-22 22:13:37 -0700

    clean up the style error for the app.js file

--- server/app.js
+++ server/app.js
@@ -1,59 +1,62 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3007;
 const db = require('../db/index.js');
 
 // Serve up the public folder since that's where our client bundle.js file will end up.
-app.use(express.static(__dirname + '/../client/dist'));
+// app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-let Product = db.Product;
-let ProductModel = db.ProductModel;
+// const Product = db.Product;
+// const ProductModel = db.ProductModel;
+const { Product, ProductModel } = db.SuggestProduct;
+// const SuggestProduct = db.SuggestProduct;
+// const SuggestProductModel = db.SuggestProductModel;
 
-let SuggestProduct = db.SuggestProduct;
-let SuggestProductModel = db.SuggestProductModel;
+const { SuggestProduct, SuggestProductModel } = db.SuggestProduct;
 
-app.get('/api/products', (req, res)=> {
-	ProductModel.all(Product, (err, resData)=> {		
-		if(err) {
-			res.send('err');
-		} else {
-			res.send(resData);
-		}		
-	})
+app.get('/api/products', (req, res) => {
+  ProductModel.all(Product, (err, resData) => {
+    if (err) {
+      res.send('err');
+    } else {
+      res.send(resData);
+    }
+  });
 });
 
-app.get('/api/suggestProducts/:id', (req, res) => {	
-	const id = req.params.id;  //'5bc966caa6944b44e5edf886'
-	console.log(id)
-	SuggestProductModel.get(SuggestProduct, id, (err, resData) =>{
-		if(err) {
-			res.send('err');
-		} else {
-			res.send(resData);
-		}
-	})
-})
+app.get('/api/suggestProducts/:id', (req, res) => {
+  // '5bc966caa6944b44e5edf886'
+  const { id } = req.params.id;
+  SuggestProductModel.get(SuggestProduct, id, (err, resData) => {
+    if (err) {
+      res.send('err');
+    } else {
+      res.send(resData);
+    }
+  });
+});
 
 app.get('/api/suggestProducts', (req, res) => {
-	console.log('here?')
-	SuggestProductModel.all(SuggestProduct, (err, resData)=> {
-		if(err) {
-			res.send('err');
-		} else {
-			res.send(resData);
-		}		
-	})
+  SuggestProductModel.all(SuggestProduct, (err, resData) => {
+    if (err) {
+      res.send('err');
+    } else {
+      res.send(resData);
+    }
+  });
 });
 
-
 app.get('/', (req, res) => {
-	res.send('Hello world!');
+  res.send('Hello world!');
 });
 
 if (process.env.NODE_ENV !== 'test') {
-	app.listen(port, () => {
-		console.log(`Exapmle app listening on port ${port}!`);
-	})	
+  app.listen(port, () => {
+    console.log(`Exapmle app listening on port ${port}!`);
+  });
 }
 
 module.exports = app;
