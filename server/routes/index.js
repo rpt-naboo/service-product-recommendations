971c9242efbcaa13925a70262eb4e5a39e518558
montekaka <josh.chen.jia@gmail.com>
2018-10-24 17:55:57 -0700

    add routes for product and suggestion

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const productsController = require('../controllers').products;
+const suggestionsController = require('../controllers').suggestions;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status.send({
+    message: 'Welcome to the Naboo API!',
+  }));
+  
+  app.get('/api/products', productsController.list);
+  app.post('/api/products', productsController.create);
+  app.get('/api/suggestions', suggestionsController.list);
+};
