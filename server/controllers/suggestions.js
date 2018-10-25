4915d03d1d538c4ed693040a2deba6cdc4533fd2
montekaka <josh.chen.jia@gmail.com>
2018-10-24 18:16:36 -0700

    add get request for suggestions

--- server/controllers/suggestions.js
+++ server/controllers/suggestions.js
@@ -7,4 +7,10 @@ module.exports = {
       .then(suggestions => res.status(200).send(suggestions))
       .catch(error => res.status(400).send(error));
   },
+  get(req, res) {
+    return Suggestion
+      .findAll({where: {productId: req.params.productId}})
+      .then(suggestions => res.status(200).send(suggestions))
+      .catch(error => res.status(400).send(error));
+  }
 };
