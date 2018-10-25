dedcd407d748f0611756cd0fc6a193aab29d9efb
montekaka <josh.chen.jia@gmail.com>
2018-10-24 21:26:52 -0700

    write test to test suggestions endpoint

--- server/app.test.js
+++ server/app.test.js
@@ -1,10 +1,18 @@
 const request = require('supertest');
+const chai = require('chai');
+const app = require('./app');
 
-const app = require('./app.js');
+const expect = chai.expect;
 
-describe('Gets api/suggestProducts/id', () => {
-  it('should return suggests products', async () => {
-    const res = await request(app).get('/api/suggestProducts/5bc966caa6944b44e5edf886');
-    expect((res.body.length)).toBe(9);
+describe('Suggestion GET API Test', () => {
+  describe('#GET / api/suggestions/products/:id', () => {
+    it('should get all suggestions', (done) => {
+      request(app).get('/api/suggestions/products/1')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body.length).to.equal(5);
+          done();
+        });
+    });
   });
 });
