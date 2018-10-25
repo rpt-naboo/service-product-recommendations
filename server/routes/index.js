16a16356cbda49d2f2314aee54df6d505bf5e0cb
montekaka <josh.chen.jia@gmail.com>
2018-10-24 18:18:18 -0700

    add product and suggestion endpoint route

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,5 +8,7 @@ module.exports = (app) => {
   
   app.get('/api/products', productsController.list);
   app.post('/api/products', productsController.create);
+  app.get('/api/products/:id', productsController.get);
   app.get('/api/suggestions', suggestionsController.list);
+  app.get('/api/suggestions/:productId', suggestionsController.get);
 };
