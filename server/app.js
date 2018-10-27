9fa93ae865e1dba3c1527aee5beef10321510624
montekaka <josh.chen.jia@gmail.com>
2018-10-27 16:08:08 -0700

    allow cross site request

--- server/app.js
+++ server/app.js
@@ -8,19 +8,31 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = 3007;
+const port = 3000;
 // Serve up the public folder since that's where our client bundle.js file will end up.
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-require('./routes')(app);
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of naboo.',
-}));
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname , '/../client/dist/index.html'), (err) => {
+		if (err) {
+			res.status(500).send(err);
+		}
+	})
+});
 
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => {
-    console.log(`Exapmle app listening on port ${port}!`);
-  });
-}
+app.listen(port, function() {
+  console.log(`listening on port ${port}!`);
+});
 
-module.exports = app;
+// require('./routes')(app);
+// app.get('*', (req, res) => res.status(200).send({
+//   message: 'Welcome to the beginning of naboo.',
+// }));
+
+// if (process.env.NODE_ENV !== 'test') {
+//   app.listen(port, () => {
+//     console.log(`Exapmle app listening on port ${port}!`);
+//   });
+// }
+
+// module.exports = app;
